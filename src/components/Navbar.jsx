96c2a408b4b8c9257879c9664f7f5095837f13ea
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-24 16:58:29 +0100

    style: styling the navbar, choosing the colors and fonts. Creating home component.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import './Navbar.css';
+import { Link, NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (<div className="navbar">
+    <Link to={"/"}>
+      <h1>NataliaK Beauty</h1>
+    </Link>
+    <div className="links">
+      <NavLink className={({isActive}) => isActive ? "active-link" : null} to={"/cennik"}>Cennik</NavLink>
+      <NavLink className={({isActive}) => isActive ? "active-link" : null} to={"/zalecenia"}>Zalecenia</NavLink>
+      <NavLink className={({isActive}) => isActive ? "active-link" : null} to={"/regulamin"}>Regulamin</NavLink>
+      <NavLink className={({isActive}) => isActive ? "active-link" : null} to={"/ankieta"}>Ankieta</NavLink>
+      <NavLink className={({isActive}) => isActive ? "active-link" : null} to={"/potfolio"}>Portfolio</NavLink>
+    </div>
+  </div>)
+}
+
+export default Navbar;
