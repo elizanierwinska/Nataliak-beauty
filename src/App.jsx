add3d95a52fdca722acf4b2df1d658cbf636c834
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-24 21:30:20 +0100

    work on zalecenia component structure and style

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Layout from './components/Layout'
 import Cennik from './components/Cennik';
+import Zalecenia from './components/Zalecenia';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route element={<Layout />}>
         <Route path="/" element={<Home/>} />
         <Route path="/cennik" element={<Cennik />} />
+        <Route path="/zalecenia" element={<Zalecenia />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Zalecenia.jsx
+++ src/components/Zalecenia.jsx
@@ -1,5 +1,37 @@
 import './Zalecenia.css';
 
-function Zalecenia() {
-  
+export default function Zalecenia() {
+  return( 
+  <div className='zalecenia'>
+    <div className="zalecania-container">
+      <div className="zalecenia-brwi">
+        <h1>Stylizacja Brwi</h1>
+        <p><b>Laminacja brwi z farbowaniem</b></p>
+        <p>Czas wykonywania usługi: 1,5h</p>
+        <p><b>Laminacja brwi</b></p>
+        <p>Czas wykonywania usługi: 50min</p>
+        <h3>PRZECIWSKAZANIA</h3>
+        <ul>
+          <li>ciąża</li>
+          <li>sucha skura w brwiach, podrażnienia</li>
+          <li>krótki lub przycięty włos</li>
+          <li>włos cienki z ubytkami</li>
+          <li>zabiegi złuszczające przed zabiiegiem</li>
+          <li>stosowanie izoteku</li>
+        </ul>
+        <h3>WSKAZANIA</h3>
+        <ul>
+          <li>brwi trudne do ujarzmienia</li>
+          <li>brwi bez kształtu</li>
+          <li>włoski brwi są bardzo sztywne</li>
+          <li>szybko wypłukują kolor po koloryzacji brwi</li>
+          <li>mają niepoprawny kierunek</li>
+        </ul>
+        <p>*zabieg prezentuje się najkorzystniej , gdy klientka posiada pełną brew bez ubytków (szczególnie w dolnej linii brwi)</p>
+        <h3>ZALECENIA</h3>
+        <p>Po zabiegu laminacji brwi klientka powinna po 2 dni nawilać włoski olejkiem lub kremem arganowym! Brwi naley czesać i układać na MYDEŁKO lub ŻEL do brwi!</p>
+      </div>
+      <img src="./images/brow-zalecenia-1.jpeg"/>
+    </div>
+  </div>)
 }
