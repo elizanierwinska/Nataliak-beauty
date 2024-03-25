877a62fb38bc13f7975fdb3c405b74562a9e60c7
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-25 17:26:24 +0100

    style: add about me section on the home page and add the icon to the webpage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,23 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (<div className="Home">
     <div className="top-container">
-      <img className="main-img" src="./images/natalia-main.jpeg" alt="zdjęcie Natalii" />
-      <img className="second-img" src="./images/natalia.jpeg" alt="zdjęcie Natalii" />
-      <img className="third-img" src="./images/natalia-2.jpeg" alt="zdjęcie Natalii" />
-      <h1>NataliaK Beauty</h1>
-      <h2>MAKIJAŻ | STYLIZACJA BRWI I RZĘS <Link to="/cennik">→</Link></h2>
+      <div className="title-flex">  
+        <h1>NataliaK Beauty</h1>
+        <h2>MAKIJAŻ | STYLIZACJA BRWI I RZĘS <Link to="/cennik">→</Link></h2>
+      </div>
+      <div className="img">
+        <img className="main-img" src="./images/natalia-main.jpeg" alt="zdjęcie Natalii" />
+        <img className="second-img" src="./images/natalia.jpeg" alt="zdjęcie Natalii" />
+        <img className="third-img" src="./images/natalia-2.jpeg" alt="zdjęcie Natalii" />
+      </div>
     </div>
-    <div className="about-container"></div>
+    <div className="about-container">
+      <h1>O mnie</h1>
+      <div>
+        <p>Cześć! Mam na imię Natalia i profesjonalnie wykonuję makijaż od 20xx roku. Makijaż pasjonował mnie od kiedy pamiętam, z tego względu każdą wolną chwilę poświęcałam nauce nowych technik i styli.xxxxxxxx. Jeśli masz jakiekolwiek pytania <Link to="/contact">skontaktuj się ze mną</Link></p>
+        <p id="signature">Natalia K.</p>
+      </div>
+    </div>
   </div>)
 }
 
