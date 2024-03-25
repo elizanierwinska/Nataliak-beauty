e2eab6799e4cb0a9b0e31448bfd4da6a56632bf1
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-25 15:13:06 +0100

    style: add h1 and h2 title elements, make their position absolute

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import './Home.css';
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (<div className="Home">
@@ -6,7 +7,10 @@ export default function Home() {
       <img className="main-img" src="./images/natalia-main.jpeg" alt="zdjęcie Natalii" />
       <img className="second-img" src="./images/natalia.jpeg" alt="zdjęcie Natalii" />
       <img className="third-img" src="./images/natalia-2.jpeg" alt="zdjęcie Natalii" />
+      <h1>NataliaK Beauty</h1>
+      <h2>MAKIJAŻ | STYLIZACJA BRWI I RZĘS <Link to="/cennik">→</Link></h2>
     </div>
+    <div className="about-container"></div>
   </div>)
 }
 
