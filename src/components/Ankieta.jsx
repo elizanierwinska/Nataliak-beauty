9166f66193bf25be8ea278a3e20d1edcbdf2752b
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-03-26 18:00:33 +0100

    embed the google forms form in ankieta component

--- src/components/Ankieta.jsx
+++ src/components/Ankieta.jsx
@@ -2,6 +2,6 @@ import "./Ankieta.css"
 
 export default function Ankieta() {
   return <div className="ankieta">
-    This is ankieta component
+    <iframe className="ankieta-form" src="https://docs.google.com/forms/d/e/1FAIpQLSdavQLBqmw3-JXVK-E6y5GGONqpjGbnVqJek-0X42MpKmAxuQ/viewform?embedded=true" width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
   </div>
 }
