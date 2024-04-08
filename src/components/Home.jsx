34abb18c900b5acd04f0d2cff4643d98dcc5acce
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-04-08 22:11:11 +0200

    feature: work on home component responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (<div className="Home">
     <div className="top-container">
-      <div className="title-flex">  
-        <h1>NataliaK Beauty</h1>
-        <h2>MAKIJAŻ | STYLIZACJA BRWI I RZĘS <Link to="/cennik">→</Link></h2>
+      <div className="top-container-flex">
+        <img className="main-img" src="./images/natalia-main.jpeg" alt="zdjęcie Natalii" />
+        <div className="title-flex">  
+          <h1>NataliaK Beauty</h1>
+          <h2>MAKIJAŻ | STYLIZACJA BRWI I RZĘS <Link to="/cennik">→</Link></h2>
+        </div>
       </div>
       <div className="img">
-        <img className="main-img" src="./images/natalia-main.jpeg" alt="zdjęcie Natalii" />
         <img className="second-img" src="./images/natalia.jpeg" alt="zdjęcie Natalii" />
         <img className="third-img" src="./images/natalia-2.jpeg" alt="zdjęcie Natalii" />
       </div>
